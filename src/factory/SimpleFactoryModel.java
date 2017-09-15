bf82c0ed81fb191d9afd29067c85e2a1eef23c85
summer <sunmeng1995@outlook.com>
2017-09-15 11:58:08 +0800

    add FactoryMethodModel and AbstractFactoryModel

--- src/factory/SimpleFactoryModel.java
+++ src/factory/SimpleFactoryModel.java
@@ -1,5 +1,9 @@
 package factory;
 
+import factory.simplefactory.BadFruitException;
+import factory.simplefactory.Fruit;
+import factory.simplefactory.FruitFactory;
+
 /**
  * Created by Sunmeng on 2017/9/14.
  * E-Mail:Sunmeng1995@outlook.com
@@ -8,8 +12,11 @@ package factory;
 public class SimpleFactoryModel {
 
     /**
-     *
-     * */
+     * 简单工厂模式基础构成：工厂(Factory)，抽象产品(Product)，具体产品(ConcreteProduct)
+     * 工厂:提供接口，获取对象
+     * 抽象产品：抽象出不同产品的共同点，Product可以是接口或者抽象类
+     * 具体产品：继承或实现抽象产品后的实际产品
+     */
 
     public static void main(String[] args) {
         try {
